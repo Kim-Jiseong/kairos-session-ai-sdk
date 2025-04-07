94ee6f01c191aacff69815ca695d46f84daf4931
Kim-Jiseong <jiseong0173@gmail.com>
2025-04-07 18:52:49 +0900

    temp

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,9 @@
+// import { openai } from "@ai-sdk/openai";
+// import { streamText, tool } from "ai";
+// import { z } from "zod";
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+}
